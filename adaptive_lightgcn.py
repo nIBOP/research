trying to fix stop gradient with ego_embs

adaptive_lightgcn.py
@@ -157,7 +157,10 @@ class AdaptiveLightGCN(LightGCN):
         if self.centroid_proj:
             all_centroids_matrix = self.centroid_proj(all_centroids_matrix)
 
-        current_item_embs = F.normalize(pos_embeddings.detach(), dim=1)
+        # Значение при forward = pos_embeddings, градиент при backward идет только в pos_ego_embeddings
+        sg_embeddings = pos_ego_embeddings + (pos_embeddings - pos_ego_embeddings).detach()
+        current_item_embs = F.normalize(sg_embeddings, dim=1)
+        
         all_centroids_matrix = F.normalize(all_centroids_matrix, dim=1)
 
         cos_sim = torch.matmul(current_item_embs, all_centroids_matrix.t())